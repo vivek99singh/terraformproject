9492dafd78df7b2c1fd9bffdfd8800bd0621a735
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-12 17:57:11 +0200

    Update Terraform configuration

---
+++ terraform/modules/vm/variables.tf
@@ -0,0 +1,19 @@
+variable "subnet_id" {
+  type = string
+}
+
+variable "public_ip_id" {
+  type = string
+}
+
+variable "nsg_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
