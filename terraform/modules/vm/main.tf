412cb153a94d98d567c23ed60cd6a07439ad8eba
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-13 18:00:23 +0200

    Update Terraform configuration

---
+++ terraform/modules/vm/main.tf
@@ -0,0 +1,55 @@
+resource "azurerm_network_interface" "main" {
+  name                = "${var.resource_group_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = var.public_ip_id
+  }
+}
+
+resource "random_password" "admin" {
+  length  = 16
+  special = true
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "${var.resource_group_name}-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = random_password.admin.result
+  network_interface_ids = [azurerm_network_interface.main.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_managed_disk" "additional" {
+  name                 = "${var.resource_group_name}-additionaldisk"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  storage_account_type = "Premium_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 256
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "additional" {
+  managed_disk_id    = azurerm_managed_disk.additional.id
+  virtual_machine_id = azurerm_windows_virtual_machine.main.id
+  lun                = 10
+  caching            = "ReadWrite"
+}
