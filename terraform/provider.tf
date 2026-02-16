79d667c310475a2c201466b9d74fb463219d619b
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-16 23:32:00 +0200

    feat: Create separate provider.tf for Azure provider configuration
    Separated provider configuration into its own file following Terraform best practices.
    This improves code organization and maintainability.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,18 @@
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = false
+    }
+
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
