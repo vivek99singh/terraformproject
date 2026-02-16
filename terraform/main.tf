2273be4af1191f3be20c0af3df6e24bc9e541f12
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-16 23:36:09 +0200

    Update Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,13 +32,13 @@ module "network" {
 module "vm" {
   source = "./modules/vm"
   
-  resource_group_name                  = azurerm_resource_group.main.name
-  location                             = azurerm_resource_group.main.location
-  subnet_id                            = module.network.subnet_ids[0]
-  vm_size                              = var.vm_size
-  admin_username                       = var.admin_username
-  tags                                 = var.tags
-  public_ip_id                         = module.network.public_ip_id
-  nsg_id                               = module.network.nsg_id
+  resource_group_name                 = azurerm_resource_group.main.name
+  location                            = azurerm_resource_group.main.location
+  subnet_id                           = module.network.subnet_id
+  vm_size                             = var.vm_size
+  admin_username                      = var.admin_username
+  tags                                = var.tags
+  public_ip_id                        = module.network.public_ip_id
+  nsg_id                              = module.network.nsg_id
   boot_diagnostics_storage_account_uri = azurerm_storage_account.bootdiag.primary_blob_endpoint
 }
