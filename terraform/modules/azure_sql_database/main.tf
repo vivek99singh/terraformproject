149a9207c8dc872d1556c62e84a53804c01331e5
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-17 17:10:30 +0200

    Update Terraform configuration

---
+++ terraform/modules/azure_sql_database/main.tf
@@ -0,0 +1,42 @@
+resource "azurerm_mssql_server" "main" {
+  name                         = "sqlserver-${random_string.server_suffix.result}"
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = random_password.sqladmin.result
+  minimum_tls_version          = "1.2"
+  tags                         = var.tags
+}
+
+resource "random_string" "server_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "random_password" "sqladmin" {
+  length  = 16
+  special = true
+}
+
+resource "azurerm_mssql_database" "main" {
+  name                = "sqldb-${random_string.db_suffix.result}"
+  server_id           = azurerm_mssql_server.main.id
+  sku_name            = "S0"
+  max_size_gb         = 10
+  tags                = var.tags
+}
+
+resource "random_string" "db_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_ip" {
+  name      = "allow-specific-ip"
+  server_id = azurerm_mssql_server.main.id
+  start_ip_address = "89.27.102.166"
+  end_ip_address   = "89.27.102.166"
+}
--- terraform/modules/azure_sql_database/outputs.tf
+++ terraform/modules/azure_sql_database/outputs.tf
@@ -1,14 +1,14 @@
 output "sql_server_id" {
-  value       = module.azure_sql_database.sql_server_id
+  value       = azurerm_mssql_server.main.id
   description = "ID of the Azure SQL Server"
 }
 
 output "sql_database_id" {
-  value       = module.azure_sql_database.sql_database_id
+  value       = azurerm_mssql_database.main.id
   description = "ID of the Azure SQL Database"
 }
 
 output "firewall_rule_id" {
-  value       = module.azure_sql_database.firewall_rule_id
+  value       = azurerm_mssql_firewall_rule.allow_ip.id
   description = "ID of the Azure SQL Firewall Rule"
 }
