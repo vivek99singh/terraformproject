51a5caf1d7d67a72e3fc6a86480c4e5309c31357
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-17 02:09:14 +0200

    Update Terraform configuration

---
+++ terraform/modules/azure_sql_database/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_mssql_server" "main" {
+  name                         = "mssqlserver-${var.resource_group_name}"
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "ComplexPassword!23"
+  minimum_tls_version          = "1.2"
+  tags                         = var.tags
+}
+
+resource "azurerm_mssql_database" "main" {
+  name           = "sqldatabase-${var.resource_group_name}"
+  server_id      = azurerm_mssql_server.main.id
+  sku_name       = "S0"
+  max_size_gb    = 5
+  tags           = var.tags
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name                = "allow-azure-services"
+  server_id           = azurerm_mssql_server.main.id
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
