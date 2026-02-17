346643ecb4b4b6ff19df29c1d3d97d423a5e537b
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-17 16:01:56 +0200

    Update Terraform configuration

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,15 +1,14 @@
 output "subnet_id" {
-  value = values(azurerm_subnet.main)[0].id
-}
-
-output "nsg_id" {
-  value = azurerm_network_security_group.main.id
+  value       = azurerm_subnet.main[0].id
+  description = "ID of the first subnet"
 }
 
 output "public_ip_id" {
-  value = azurerm_public_ip.main.id
+  value       = azurerm_public_ip.main.id
+  description = "ID of the public IP"
 }
 
 output "public_ip_address" {
-  value = azurerm_public_ip.main.ip_address
+  value       = azurerm_public_ip.main.ip_address
+  description = "Public IP address"
 }
