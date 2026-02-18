369570efa319a029c8a8eaffd3eba0ff753a8488
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-18 13:51:33 +0200

    Update Terraform configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,14 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+  backend "azurerm" {}
+}
