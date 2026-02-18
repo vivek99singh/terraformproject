119931ffaa7b99b13154af222bf53eac5cfb2c23
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-18 12:21:01 +0200

    Update Terraform configuration

---
+++ terraform/sqlazure.tf
@@ -0,0 +1,42 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "random_password" "sql_admin_password" {
+  length  = 16
+  special = true
+}
+
+resource "azurerm_mssql_server" "main" {
+  name                         = "mssqlserver${random_string.server_suffix.result}"
+  resource_group_name          = azurerm_resource_group.main.name
+  location                     = azurerm_resource_group.main.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = random_password.sql_admin_password.result
+  minimum_tls_version          = "1.2"
+  tags                         = var.tags
+}
+
+resource "azurerm_mssql_database" "main" {
+  name                = "sqldatabase"
+  server_id           = azurerm_mssql_server.main.id
+  sku_name            = "S0"
+  max_size_gb         = 10
+  tags                = var.tags
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_ip" {
+  name             = "allow-specific-ip"
+  server_id        = azurerm_mssql_server.main.id
+  start_ip_address = "89.27.102.166"
+  end_ip_address   = "89.27.102.166"
+}
+
+resource "random_string" "server_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
