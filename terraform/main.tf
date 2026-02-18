67620c2e01f712aec8d60953d28338dcf8209152
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-18 15:36:39 +0200

    Update Terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+module "azure_sql" {
+  source = "./modules/azure-sql"
+  
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  tags                = var.tags
+}
--- terraform/modules/azure-sql/outputs.tf
+++ terraform/modules/azure-sql/outputs.tf
@@ -1,14 +1,9 @@
 output "sql_server_id" {
   value       = azurerm_mssql_server.main.id
-  description = "ID of the Azure SQL Server"
+  description = "ID of the SQL Server"
 }
 
 output "sql_database_id" {
   value       = azurerm_mssql_database.main.id
-  description = "ID of the Azure SQL Database"
-}
-
-output "sql_server_fqdn" {
-  value       = azurerm_mssql_server.main.fully_qualified_domain_name
-  description = "Fully Qualified Domain Name of the Azure SQL Server"
+  description = "ID of the SQL Database"
 }
