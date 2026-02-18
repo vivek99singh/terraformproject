e425924882024cb43458bdcfc11773f0e77c8042
Vivek Singh <vivek.99.singh@gmail.com>
2026-02-18 14:25:53 +0200

    Update Terraform configuration

--- terraform/modules/azure-sql/outputs.tf
+++ terraform/modules/azure-sql/outputs.tf
@@ -1,9 +1,9 @@
 output "sql_server_id" {
-  value       = module.azure_sql.sql_server_id
+  value       = azurerm_mssql_server.main.id
   description = "ID of the SQL Server"
 }
 
 output "sql_database_id" {
-  value       = module.azure_sql.sql_database_id
+  value       = azurerm_mssql_database.main.id
   description = "ID of the SQL Database"
 }
